798ed249c7427d6e5d131346db9c1fc582f9b80f
James Yuill <jamesyuill@jamess-mbp.lan>
2024-02-03 19:10:01 +0000

    updates circle with an animation that slowly covers the grid with characters

--- circle.py
+++ circle.py
@@ -1,11 +1,12 @@
 from random import randint
 import os
 from time import sleep
+import math
 
 
 
-rows = 50
-cols = 10
+rows = 60
+cols = 30
 
 def createGrid():
     grid = []
@@ -18,7 +19,25 @@ def createGrid():
 
 
 grid = createGrid()
-for i in range(len(grid)):
-    print(f'{"".join(grid[i])}\n')
+
+#adds shape to grid
+r = 0
+angle = 0
 
 
+
+while True:
+
+    for angle in range(2000):
+        pos_x = int(r * math.cos(angle))
+        pos_y = int(r * math.sin(angle) / 2)
+        grid[pos_y][pos_x] = '\x1b[40m'+'o'+'\x1b[0m'
+
+#print the grid
+    for i in range(len(grid)):
+        print(f'{"".join(grid[i])}')
+
+    r += 1
+
+    sleep(0.2)
+    os.system('clear')
